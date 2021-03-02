4e2adce4cbdef35aaa15a6c6015e175bc38f2185
JonatanValdivia <jonatan.viniciusvaldivia@outlook.com>
2021-03-02 15:38:13 -0300

    Projetos finalizados. Entrega.

--- SA01-RTI-SOLUTIONS/Fulbank/index.php
+++ SA01-RTI-SOLUTIONS/Fulbank/index.php
@@ -1,13 +1,10 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html lang="pt=br">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="../SA01-RTI-SOLUTIONS/style_global.css" type="text/css" rel="stylesheet">
+  <link href="../style_global.css" type="text/css" rel="stylesheet">
   <title>Aumento salarial</title>
 </head>
 <body>
--- SA01-RTI-SOLUTIONS/Seneletro/index.php
+++ SA01-RTI-SOLUTIONS/Seneletro/index.php
@@ -7,11 +7,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" rel="stylesheet" href="../SA01-RTI-SOLUTIONS/style_global.css"/>
+    <link type="text/css" rel="stylesheet" href="../style_global.css"/>
     <title>onversor</title>
 </head>
 <body>
-    <form method="GET" action="../SA03-02/coversor.php">
+    <form method="GET" action="coversor.php">
     <input type="number" step="0.01" name="Real" placeholder="R$">
         <input type="radio" name="Converter" value="Dolar"/>Dolar
         <input type="radio" name="Converter" value="Euros"/>Euros
